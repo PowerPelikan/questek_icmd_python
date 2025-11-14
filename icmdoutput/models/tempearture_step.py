61d5747f7f8e81c594e0293b7d5e6e7c6b552fd2
jonahart <jhartmann@LWK-Hartmann-Jonathan.ad.uni-paderborn.de>
2025-11-14 11:22:00 +0100

    changed comment

--- icmdoutput/models/tempearture_step.py
+++ icmdoutput/models/tempearture_step.py
@@ -5,6 +5,4 @@ from icmdoutput.models.equilibrium import Equilibrium
 
 class TemperatureStep(Equilibrium):
     ''' Get data from Temperautre Step model in usefull dataframes'''
-    # system_volume, molar_volume, system_densitiy, desitiy, solvus, liquidus, solidus, system_thermal_conductivity, system_compostition, pressure, system_size
-    # system_enthalpy, enthalpy, system_electrical_resistivity, electrical_resitivity, surface_tension, dynamic_viscosity, tracer_diffusion_coefficient
 
