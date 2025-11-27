1623d7ae5f47a739e4a324babc1c5eeb3896117e
PowerPelikan <jona.hart.97@proton.me>
2025-11-27 15:58:38 +0100

    Fixing paraemter for phase_fraction and Temperature

--- icmdoutput/redundant_data.py
+++ icmdoutput/redundant_data.py
@@ -12,19 +12,30 @@ class PhasesAndTemps(SingleModel):
     def get_phase_names(self):
         return self.data["coords"]["phase"]["data"]
 
-    def _get_temperatures(self, unit: str):
+    def _get_temperatures(self, unit: str, parameter: bool):
         try:
-            temps = self.data["data_vars"]["temperature_values"]["data"][0]
+            temps = self.data["data_vars"]["temperature_values"]["data"]
         except KeyError:
-            temps = self.data["data_vars"]["temperature"]["data"][0]
+            temps = self.data["data_vars"]["temperature"]["data"]
+        
+        if parameter:
+            match unit:
+                case "C":
+                    return [[row[0] for row in block] for block in temps]
+                case "K":
+                    return [[row[1] for row in block] for block in temps]
+                case "F":
+                    return [[row[2] for row in block] for block in temps]
+                case _:
+                    raise ValueError(f"Unknown temperature unit '{unit}'")
 
         match unit:
             case "C":
-                return [t[0] for t in temps]
+                return [t[0][0] for t in temps]
             case "K":
-                return [t[1] for t in temps]
+                return [t[0][1] for t in temps]
             case "F":
-                return [t[2] for t in temps]
+                return [t[0][2] for t in temps]
             case _:
                 raise ValueError(f"Unknown temperature unit '{unit}'")
 
@@ -97,30 +108,32 @@ class PhasesAndTemps(SingleModel):
     def get_phase_fraction(self, phase_unit="mole", temp_unit="C", parameter=False):
         """Return DataFrame with phase fractions."""
         phase_values = self._get_phase_fraction(phase_unit, parameter)
-        temps = pd.DataFrame(self._get_temperatures(temp_unit), columns=[f"Temperature in {temp_unit}"])
+        #temps = pd.DataFrame(self._get_temperatures(temp_unit), columns=[f"Temperature in {temp_unit}"])
+        temps = self._get_temperatures(temp_unit, parameter)
         phases = self.get_phase_names()
 
         if parameter:
             components = self.get_components()
             df_all = []
-            for params, values in zip(components.values, phase_values):
+            for params, temp_values, values in zip(components.values, temps, phase_values):
                 pa_df = pd.DataFrame([params] * len(temps), columns=components.columns)
+                tm_df = pd.DataFrame(temp_values, columns=[f"Temperature in {temp_unit}"])
                 pf_df = pd.DataFrame(values, columns=phases)
-                df = pd.concat([pa_df, temps, pf_df], axis=1)
+                df = pd.concat([pa_df, tm_df, pf_df], axis=1)
                 df_all.append(df)
             return pd.concat(df_all, ignore_index=True)
 
         phase_df = pd.DataFrame(phase_values, columns=phases)
         return pd.concat([temps, phase_df], axis=1)
 
-    def get_volume_fraction(self, temp_unit="C"):
+    def get_volume_fraction(self, temp_unit="C", parameter=False):
         """Return DataFrame with volume fractions."""
         volume_values = self._get_volume_fraction()
-        temps = pd.DataFrame(self._get_temperatures(temp_unit), columns=[f"Temperature in {temp_unit}"])
+        temps = pd.DataFrame(self._get_temperatures(temp_unit, parameter), columns=[f"Temperature in {temp_unit}"])
         phase_df = pd.DataFrame(volume_values, columns=self.get_phase_names())
         return pd.concat([temps, phase_df], axis=1)
 
-    def get_temperatures(self, unit="C"):
+    def get_temperatures(self, unit="C", parameter=False):
         """Return temperature values."""
-        temps = self._get_temperatures(unit)
+        temps = self._get_temperatures(unit, parameter)
         return pd.DataFrame(temps, columns=[f"Temperature in {unit}"])
